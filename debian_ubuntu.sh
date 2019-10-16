fa418a0fa6591fb2c0e4b6c089bb87df71b37cf1
maaaaz <maaaaz@androwarn.com>
2019-10-16 18:01:46 -0400

    new packages

--- debian_ubuntu.sh
+++ debian_ubuntu.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 apt update
-apt install -y sudo wget mlocate git kbd vim strace ltrace git net-tools lsof python-pip python3-pip dkms make bzip2 curl dnsutils openssh-server openssh-client bmon nmon htop gzip coreutils dos2unix util-linux iptables jq less nmap parallel sed grep sqlite3 ftp tzdata vlan python-virtualenv virtualenvwrapper libpq-dev libpcap-dev libssl-dev python3 python3-dev
+apt install -y sudo wget mlocate git kbd vim strace ltrace git net-tools lsof python-pip python3-pip dkms make bzip2 curl dnsutils openssh-server openssh-client bmon nmon htop gzip coreutils dos2unix util-linux iptables jq less nmap parallel sed grep sqlite3 ftp tzdata vlan python-virtualenv virtualenvwrapper libpq-dev libpcap-dev libssl-dev python3 python3-dev xxd colordiff findutils zutils nfs-common ntfs-3g pigz freerdp xrdp cifs-utils smbclient openssl subversion tar telnet tcpdump tshark traceroute vbindiff usbutils whois xz-utils
 
 # set permanently the keyboard mapping
 localectl set-keymap fr
