20a977f2314becd11713e8942e8d059ad46a9a19
maaaaz <maaaaz@androwarn.com>
2019-10-19 13:11:33 -0400

    fuse fix

--- centos_rhel.sh
+++ centos_rhel.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-yum install -y epel-release sudo wget git kbd vim vim-common strace ltrace git net-tools lsof python2-pip python34-pip dkms make bzip2 curl bind-utils openssh bmon nmon htop gzip coreutils dos2unix util-linux iptables jq less nmap-ncat parallel sed grep sqlite ftp tzdata vconfig python-virtualenv python-virtualenvwrapper postgresql-devel libpcap-devel python36 python36-devel colordiff findutils nfs-utils ntfs-3g pigz freerdp xrdp samba samba-client cifs-utils openssl subversion tar telnet tcpdump wireshark traceroute usbutils whois xz
+yum install -y epel-release sudo wget git kbd vim vim-common strace ltrace git net-tools lsof python2-pip python34-pip dkms make bzip2 curl bind-utils openssh bmon nmon htop gzip coreutils dos2unix util-linux iptables jq less nmap-ncat parallel sed grep sqlite ftp tzdata vconfig python-virtualenv python-virtualenvwrapper postgresql-devel libpcap-devel python36 python36-devel colordiff findutils nfs-utils ntfs-3g pigz freerdp xrdp samba samba-client cifs-utils openssl subversion tar telnet tcpdump wireshark traceroute usbutils whois xz fuse
 
 # set permanently the keyboard mapping
 localectl set-keymap fr
