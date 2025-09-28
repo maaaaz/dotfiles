3f513e047a014e955683a66bb88f3f6c02b88e47
Thomas D. <maaaaz@users.noreply.github.com>
2025-09-28 13:33:05 +0200

    Update centos_rhel.sh

--- centos_rhel.sh
+++ centos_rhel.sh
@@ -14,7 +14,7 @@ echo 'eval "`dircolors`"' >> ~/.bashrc
 echo alias ls='ls $LS_OPTIONS' >> ~/.bashrc
 
 # upgrade pip and install cool modules
-pip install --upgrade pip virtualenvwrapper csvkit pyinstaller
+pip install -q --upgrade pip virtualenvwrapper csvkit pyinstaller
 
 # set up virtualenvwrapper
 grep -i 'workon_home' ~/.bashrc ||  echo 'export WORKON_HOME=~/.virtualenvs' >> ~/.bashrc
