1f97c099dce84709388f15b7c8e2195d1b7af5af
Thomas D. <maaaaz@users.noreply.github.com>
2025-11-01 14:35:58 +0100

    Update centos_rhel.sh

--- centos_rhel.sh
+++ centos_rhel.sh
@@ -9,7 +9,7 @@ dnf install -y --best --allowerasing $(cat "./centos_rhel_packages.txt") || (ech
 
 # some missing packages that might be fixed at some point
 dnf install -y dc3dd fd-find fuse-sshfs ioping jhead rclone xrdp || true
-dnf install -y plocate || true
+dnf install -y plocate xxd || true
 
 # set permanently the keyboard mapping
 localectl set-keymap fr || true
