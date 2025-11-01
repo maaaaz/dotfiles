f99b2f0db058915aad274711d0107afba014cc74
Thomas D. <maaaaz@users.noreply.github.com>
2025-11-01 15:09:17 +0100

    Update kali_missing.sh

--- kali_missing.sh
+++ kali_missing.sh
@@ -1,3 +1,6 @@
-#!/bin/sh
+#!/usr/bin/env bash
+set -xe
+
 apt update
-apt install -y $(cat "./kali_missing_packages.txt")
+apt install -y $(cat "./kali_missing_packages.txt") || (echo "[!] issue while installing required packages !" && exit 1)
+apt clean && apt autoclean
