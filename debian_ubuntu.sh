3ad3d33c7a574f99d2bec6b29c029819674a8a55
Thomas D. <maaaaz@users.noreply.github.com>
2025-11-01 14:58:45 +0100

    Update debian_ubuntu.sh

--- debian_ubuntu.sh
+++ debian_ubuntu.sh
@@ -1,21 +1,22 @@
-#!/bin/sh
+#!/usr/bin/env bash
+set -xe
+
 apt update
 apt install -y $(cat "./debian_ubuntu_packages.txt") || (echo "[!] issue while installing required packages !" && exit 1)
 
 # packages that do not exist in every debian-based distros (for instance: kali)
-apt install -y mlocate
+apt install -y mlocate || true
 
 # set permanently the keyboard mapping
-localectl set-keymap fr
+localectl set-keymap fr || true
+
+# upgrade pip and install cool modules
+python -m pip install -q --upgrade pip csvkit pyinstaller
 
 # customizing bashrc
 echo "export LS_OPTIONS='--color=auto'" >> ~/.bashrc
 echo 'eval "`dircolors`"' >> ~/.bashrc
 echo alias ls='ls $LS_OPTIONS' >> ~/.bashrc
-
-# upgrade pip and install cool modules
-pip2 install csvkit pyinstaller
-pip3 install csvkit pyinstaller
 
 # set up virtualenvwrapper
 grep -i 'workon_home' ~/.bashrc ||  echo 'export WORKON_HOME=~/.virtualenvs' >> ~/.bashrc
@@ -24,8 +25,10 @@ grep -i 'virtualenvwrapper.sh' ~/.bashrc ||  echo 'source /usr/share/virtualenvw
 
 # install vmware tools and setup shared folder
 apt install -y build-essential unzip patch gcc linux-headers-$(uname -r) perl psmisc zip fuse3
-apt install -y open-vm-tools &&\
-echo ".host:/PartageVM     $HOME/PartageVM    fuse.vmhgfs-fuse       defaults,auto,nofail,allow_other    0       0" >> /etc/fstab
+if dmidecode | grep -qi 'vmware'; then
+  apt install -y open-vm-tools &&\
+  echo ".host:/PartageVM     $HOME/PartageVM    fuse.vmhgfs-fuse       defaults,auto,nofail,allow_other    0       0" >> /etc/fstab
+fi
 
 # setting timezone
 rm /etc/localtime
