9dc20a1278e79a42b5b47e05272cfdad597cd65f
Thomas D. <maaaaz@users.noreply.github.com>
2026-05-30 14:03:27 +0200

    Create coreelec.sh

---
+++ coreelec.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+set -xe
+
+opkg update
+opkg install -y $(cat "./coreelec_packages.txt") || (echo "[!] issue while installing required packages !" && exit 1)
+
+# disabling unused services
+SERVICES_TO_DISABLE="connman-vpn.service nfs-idmapd.service nfs-mountd.service nfsdcld.service rpc-statd.service rpc-statd-notify.service rpcbind.service samba-config.service wsdd2.service vfd-clock.service rpcbind.socket"
+systemctl stop $SERVICES_TO_DISABLE
+systemctl mask $SERVICES_TO_DISABLE
