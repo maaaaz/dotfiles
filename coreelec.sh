b4080857a03b3ccb20ad28eaff2f3ca3626dfca7
Thomas D. <maaaaz@users.noreply.github.com>
2026-05-31 14:44:11 +0200

    Update coreelec.sh

--- coreelec.sh
+++ coreelec.sh
@@ -2,6 +2,7 @@
 set -xe
 
 opkg update
+opkg remove ncat aria2
 opkg install $(cat "./coreelec_packages.txt") || (echo "[!] issue while installing required packages !" && exit 1)
 
 # disabling unused services
